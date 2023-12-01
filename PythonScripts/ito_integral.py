175016c8866bd99450b25a8cce6804c39449ca74
Dust1920 <a218208885@gmail.com>
2023-12-01 11:34:45 -0700

    isometry

--- PythonScripts/ito_integral.py
+++ PythonScripts/ito_integral.py
@@ -20,6 +20,27 @@ def ito_n(n_points: int, t: float):
     time, w = bw.u(t, n_points)
     integral = np.zeros(n_points)
     for i in range(n_points - 1):
-        integral[i] = fB(time, w[i], time[i]) * (w[i + 1] - w[i])
+        integral[i] = fB(time, time[i], time[i]) * (w[i + 1] - w[i])
     ito = integral.sum()
     return w, ito
+
+
+samples = 100
+points = 100
+
+time = 1
+
+t, w = bw.u(time, points)
+
+sIto_n = 0
+for i in range(samples):
+    A = 0
+    for j in range(points - 1):
+        I_i = t[j] * (w[j + 1] - w[j])
+        A += I_i
+    A = A ** 2
+    sIto_n += A
+
+print(sIto_n * samples ** (-1))
+
+
